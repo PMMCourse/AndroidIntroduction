4b7d82a9068eebdab838f2a6a3c561e38bd9f638
Jose <jmmontero.ortega@gmail.com>
2019-12-21 10:00:44 +0100

    Navigate another activity

--- AndroidIntroduction/MainActivity.cs
+++ AndroidIntroduction/MainActivity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Android.App;
+using Android.Content;
 using Android.OS;
 using Android.Runtime;
 using Android.Support.Design.Widget;
@@ -16,6 +17,7 @@ namespace AndroidIntroduction
         private Button _buttonChangeName;
         private TextView _labelChangedName;
         private EditText _editTextForName;
+        private Button _navigateSecondActivity;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -31,8 +33,17 @@ namespace AndroidIntroduction
             _buttonChangeName = FindViewById<Button>(Resource.Id.buttonChangeName);
             _labelChangedName = FindViewById<TextView>(Resource.Id.labelForName);
             _editTextForName = FindViewById<EditText>(Resource.Id.editTextName);
+            _navigateSecondActivity = FindViewById<Button>(Resource.Id.buttonNavigateSecondActivity);
+
 
             _buttonChangeName.Click += ChangeNameClick;
+            _navigateSecondActivity.Click += NavigateSecondActivityClick;
+        }
+
+        private void NavigateSecondActivityClick(object sender, EventArgs e)
+        {
+            Intent navigateIntent = new Intent(this, typeof(SecondActivity));
+            StartActivity(navigateIntent);
         }
 
         private void ChangeNameClick(object sender, EventArgs e)
